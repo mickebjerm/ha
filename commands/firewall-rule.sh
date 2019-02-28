ff10ab386446305656436048ecab4016db0e6347
mickebjerm <mikael@bjermkvist.se>
2019-02-28 05:08:49 +0100

    brp

--- commands/firewall-rule.sh
+++ commands/firewall-rule.sh
@@ -2,20 +2,21 @@
 
 SSH_USER="homeassistant"
 SSH_HOST="router.local"
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
 
 if [ "$1" == "firewall" ]; then
     if [ ! "$2" ] ; then
-        echo "ERROR: [ruletype] $(date)" >> /config/firewall-kids.log
+        echo "ERROR: [ruletype] [${DATE}]" >> /config/firewall-kids.log
         exit 0
         if [ ! "$3" ] ; then
-            echo "ERROR:[rulenumber] $(date)" >> /config/firewall-kids.log
+            echo "ERROR: [rulenumber] [${DATE}]" >> /config/firewall-kids.log
             exit 0
         fi
     else 
-        echo "INFO: [${2} rule:${3}] $(date) ${2} " >> /config/firewall-kids.log
+        echo "INFO: [${2} rule:${3}] [${DATE}] ${2} " >> /config/firewall-kids.log
         #ssh ${SSH_USER}@${SSH_HOST} -- "ip firewall filter ${2} numbers=${3}"
     fi
 else
-    echo "ERROR: [firewall] $(date)" >> /config/firewall-kids.log
+    echo "ERROR: [firewall] [${DATE}]" >> /config/firewall-kids.log
 fi
 
