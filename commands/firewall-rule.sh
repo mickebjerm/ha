5fa6347af8c0f15e629a8982dc7b58f9aaeaf367
mickebjerm <mikael@bjermkvist.se>
2019-03-26 18:32:20 +0100

    internet

--- commands/firewall-rule.sh
+++ commands/firewall-rule.sh
@@ -18,6 +18,14 @@ if [ "$1" == "firewall" ]; then
         ssh -o stricthostkeychecking=no -i ${SSH_KEY} ${SSH_USER}@${SSH_HOST} -- "ip firewall filter ${2} numbers=${3}" >> /config/firewall-kids.log
         exit 0
     fi
+elif  [ "$1" == "status" ]; then
+    echo "INFO: [status rule:${3}] [${DATE}] " >> /config/firewall-kids.log
+    STATUS=`ssh -o stricthostkeychecking=no -i ${SSH_KEY} ${SSH_USER}@${SSH_HOST} -- "ip firewall filter print " | grep ^${3} | awk '{print $2}'`
+    if [ "$STATUS" == "X" ]; then
+        echo "off"
+    else
+        echo "on" 
+    fi
 else
     echo "ERROR: [firewall] [${DATE}]" >> /config/firewall-kids.log
     exit 0
